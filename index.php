637420920dc531c1082a4c03cce6c1572f7d8c7c
shamim66ewu <shamim66ewu@gmail.com>
2024-01-15 01:48:17 +0600

    sample index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Default Page</title>
+</head>
+<body>
+  <h1>Cooming Soon!</h1>
+  <p>PHP version: <?php echo phpversion(); ?></p>
+</body>
+</html>
